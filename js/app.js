f19bd288375ba36b82ece626dd4eee5b1bc379f0
Dmitriy <deorel251098@gmail.com>
2021-10-29 19:45:46 +0700

    Сделан блок sightseens, доделана навигация, исправлены некоторые элементы welcome

--- js/app.js
+++ js/app.js
@@ -32,3 +32,47 @@ const closeModalButton = document.querySelector('.modalWindow__closeButton');
       appBody.style.overflow = 'visible';
    });
   }
+
+  const smoothLinks = document.querySelectorAll('.nav__link[href^="#"]');
+  const smoothFollowArrow = document.querySelector('.welcome__arrowLink[href^="#"]');
+  for (let smoothLink of smoothLinks) {
+      smoothLink.addEventListener('click', function (e) {
+        let modalWindow = document.querySelector('.modalWindow');
+        let appBody = document.querySelector('.app');
+        let header = document.querySelector('.header');
+          e.preventDefault();
+          const id = smoothLink.getAttribute('href');
+  
+          document.querySelector(id).scrollIntoView({
+              behavior: 'smooth',
+              block: 'start'
+          });
+          modalWindow.classList.remove('modalWindow--active');
+          header.style.visibility = 'visible'
+          appBody.style.overflow = 'visible';
+      });
+  };
+  smoothFollowArrow.addEventListener('click', function(e){
+    e.preventDefault();
+    const id = smoothFollowArrow.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+  })
+
+  window.addEventListener('scroll', event => {
+    let navigationLinks = document.querySelectorAll('.nav__link');
+    let fromTop = window.scrollY;
+    navigationLinks.forEach(link => {
+      let section = document.querySelector(link.hash);
+      if (
+        section.offsetTop <= fromTop &&
+        section.offsetTop + section.offsetHeight > fromTop
+      ) {
+        link.classList.add('nav__link--active');
+      } else {
+        link.classList.remove('nav__link--active');
+      }
+    });
+  });
